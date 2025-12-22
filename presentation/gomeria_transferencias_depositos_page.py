b417a9f3e94bd559e679b1cbae6ebf23fa69e8f5
Pc Repuestos 01 <agustinmarino38@gmail.com>
2025-12-22 13:56:06 -0300

    agregado un container para mejor visualizacion

--- presentation/gomeria_transferencias_depositos_page.py
+++ presentation/gomeria_transferencias_depositos_page.py
@@ -14,27 +14,28 @@ def gomeria_transferencias_entre_depositos() -> None:
     select = SelectBoxComponents()
     st.title(PAG_TRANSFERENCIAS_ENTRE_DEPOSITOS)
 
-    transfer, diferencia = st.columns([0.5,1])
+    with st.container(height=770):
+        transfer, diferencia = st.columns([0.5, 1])
 
-    with transfer:
-        aux, izq, der = st.columns([0.6, 1,0.6])
+        with transfer:
+            aux, izq, der = st.columns([0.55, 1,0.6])
 
-        cabecera = select.select_box_cabecera(izq, "CABECERA_TRANSFERENCIA")
-        fig_transfer = TransferenciasEntreDepositosPlotter(cabecera).create_plot()
+            cabecera = select.select_box_cabecera(izq, "CABECERA_TRANSFERENCIA")
+            fig_transfer = TransferenciasEntreDepositosPlotter(cabecera).create_plot()
 
-        with st.container(height=600):
-            if fig_transfer is not None:
-                st.plotly_chart(fig_transfer)
+            with st.container(height=601):
+                if fig_transfer is not None:
+                    st.plotly_chart(fig_transfer)
+                else:
+                    st.write("Selecciona una cabecera que contenga información.")
+
+        with diferencia.container(height=GOMERIA_BOX_HEIGHT):
+            fig_diferencia = DiferenciaMovimientosEntreDepositosPlotter().create_plot()
+
+            if fig_diferencia is not None:
+                st.plotly_chart(fig_diferencia)
             else:
-                st.write("Selecciona una cabecera que contenga información.")
-
-    with diferencia.container(height=GOMERIA_BOX_HEIGHT):
-        fig_diferencia = DiferenciaMovimientosEntreDepositosPlotter().create_plot()
-
-        if fig_diferencia is not None:
-            st.plotly_chart(fig_diferencia)
-        else:
-            st.write("No existen datos de diferencia.")
+                st.write("No existen datos de diferencia.")
 
 
 
