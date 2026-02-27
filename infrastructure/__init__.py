d071af8ddccdb33318dc25ce52dc92daece35316
Pc Repuestos 01 <agustinmarino38@gmail.com>
2026-02-27 10:55:18 -0300

    agregado cache a las conexiones a la base de datos

--- infrastructure/__init__.py
+++ infrastructure/__init__.py
@@ -1,14 +1,23 @@
+import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from config.constants_common import DB_PATH
 
-db_engine = create_engine(DB_PATH, echo=False)
-
-SessionDB = sessionmaker(bind=db_engine)
-
 DBBase = declarative_base()
+
+@st.cache_resource
+def get_db_engine():
+    return create_engine(DB_PATH, echo=False)
+
+@st.cache_resource
+def get_db_session(_engine):
+    return sessionmaker(bind=_engine)
+
+db_engine = get_db_engine()
+SessionDB = get_db_session(db_engine)
+
 
 
 
