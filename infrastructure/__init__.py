a226ad80f39683504215243566916745caa75736
Pc Repuestos 01 <agustinmarino38@gmail.com>
2026-07-08 10:44:49 -0300

    .3

--- infrastructure/__init__.py
+++ infrastructure/__init__.py
@@ -15,6 +15,7 @@ def get_db_engine_sqlite():
 def get_db_engine_postgres():
     return create_engine(
         DB_PATH_POSTGRES,
+        pool_pre_put=True,
         echo=False,
         connect_args={'options': '-csearch_path=estadisticas,datos'}
     )
